4b09405d09741e0e8f16591356d2c1a235070b11
dericksonmark <68699877+dericksonmark@users.noreply.github.com>
2020-10-28 16:07:28 -0400

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
 cd ~/Desktop/ipremover/input
-ls
 echo "Extracting files..."
 gzip -dk *.gz
 mv *.log ../logs
-cd ..
-cd logs
+cd ../logs
 sed -i "" 's/[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}/x.x.x.x/g' *.log
 echo "done!"
